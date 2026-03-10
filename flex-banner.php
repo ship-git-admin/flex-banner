61d5610b636c625b7fbe69d27199f381472b88f3
SHIP Sato <k-sato@shipinc.biz>
2026-03-10 20:32:24 +0900

    chore: 更新テストのためバージョンアップ (v2.0.8)

--- flex-banner.php
+++ flex-banner.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: フレキシブルバナーグループ
  * Description: 柔軟なバナー管理プラグイン。グリッド/スライダー表示・テキストオーバーレイ・表示スケジュール・複製機能搭載。ショートコード [flex_banner id="POST_ID"] で表示。
- * Version: 2.0.7
+ * Version: 2.0.8
  * Author: Custom
  */
 
